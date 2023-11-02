1a845a7e3429fbde2b21263fb543f9bdbd636973
Good_Gamer <1214542997@qq.com>
2023-11-02 20:28:50 +0800

    Initial Commit

---
+++ book/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  book
+//
+//  Created by 李梓豪 on 2023/11/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
